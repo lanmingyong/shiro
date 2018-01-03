45f2617b808513ea54a930ab7325d9442c51b5b8
LANMINGYONG <851762662@qq.com>
2018-01-03 21:22:10 +0800

    添加AOP代码

---
+++ masterpiece/src/main/java/org/lan/spring/aop/LogHandler.java
@@ -0,0 +1,14 @@
+package org.lan.spring.aop;
+
+public class LogHandler
+{
+    public void LogBefore()
+    {
+        System.out.println("Log before method");
+    }
+    
+    public void LogAfter()
+    {
+        System.out.println("Log after method");
+    }
+}
